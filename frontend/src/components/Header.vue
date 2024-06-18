ee6e5a89f384c7f50b90908768c297e70949b732
Thomas NACHAR <thomas.nachar18@gmail.com>
2024-06-18 22:41:23 +0200

    modal de login

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,14 +1,49 @@
 <template>
-  <header class="p-8 w-full bg-primary">
+  <header
+    class="fixed p-8 w-full bg-primary flex justify-between items-center z-30"
+  >
     <div>
-      <RouterLink to="/"
-        ><img class="h-24" src="../assets/logo.png" alt="logo"
-      /></RouterLink>
+      <RouterLink to="/">
+        <img class="h-24" src="../assets/logo.png" alt="logo" />
+      </RouterLink>
     </div>
+    <nav>
+      <div class="relative group">
+        <button class="group nav-button">
+          <span>Compte</span>
+        </button>
+        <div
+          class="absolute p-8 bg-white rounded-md w-full left-0 opacity-0 group-hover:opacity-100 z-50"
+          style="transition: opacity 0.3s ease-in-out"
+        >
+          <div
+            class="absolute w-0 h-0 border-l-8 border-r-8 border-b-8 border-transparent border-b-white top-0 left-1/2 transform -translate-x-1/2 -translate-y-full"
+          ></div>
+          <div class="flex flex-col justify-center items-center gap-4">
+            <button class="" @click="isLogin = true">Se connecter</button>
+            <button @click="isLogin = false">S'inscrire</button>
+          </div>
+        </div>
+      </div>
+    </nav>
   </header>
+  <Login :isLogin @update:isLogin="isLogin = false" />
 </template>
 
 <script lang="ts">
 import { RouterLink, RouterView } from "vue-router"
-export default {}
+import Login from "../views/users/Login.vue"
+
+export default {
+  components: {
+    Login,
+  },
+  data() {
+    return {
+      isLogin: false,
+    }
+  },
+}
 </script>
+
+<style></style>
---
+++ frontend/src/views/users/Login.vue
@@ -0,0 +1,59 @@
+<template>
+  <section
+    class="fixed inset-0 flex justify-center items-center z-40"
+    v-if="isLogin"
+  >
+    <div
+      class="fixed bg-black opacity-40 inset-0 z-40 cursor-pointer"
+      @click="closeLoginModal"
+    ></div>
+    <div class="p-8 w-[350px] bg-white z-50 animation-modal rounded-md">
+      <p class="text-3xl font-bold text-center mb-8">Connexion</p>
+      <form @submit.prevent="handleLogin" class="flex flex-col gap-4">
+        <div>
+          <label for="email">Adresse e-mail:</label>
+          <input
+            class="input"
+            type="email"
+            name="email"
+            v-model="formData.email"
+          />
+        </div>
+        <div>
+          <label for="password">Mot de passe:</label>
+          <input
+            class="input"
+            type="password"
+            name="password"
+            v-model="formData.password"
+          />
+        </div>
+        <button type="submit" class="button">Se connecter</button>
+      </form>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      formData: {
+        email: "",
+        password: "",
+      },
+    }
+  },
+  props: {
+    isLogin: Boolean,
+  },
+  methods: {
+    closeLoginModal() {
+      this.$emit("update:isLogin", false)
+    },
+    handleLogin() {
+        console.log(this.formData)
+    },
+  },
+}
+</script>
